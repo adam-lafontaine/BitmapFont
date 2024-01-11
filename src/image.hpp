f17094174ee430c80e0220901525b77ae54d4b64
adam-lafontaine <adam.lafontaine@gmail.com>
2024-01-11 12:01:29 -0500

    add code

---
+++ src/image.hpp
@@ -0,0 +1,186 @@
+#pragma once
+
+#include "types.hpp"
+
+#include <functional>
+
+template <class F>
+using fn = std::function<F>;
+
+
+/*  image types */
+
+namespace image
+{
+    class RGBAu8
+    {
+    public:
+        u8 red;
+        u8 green;
+        u8 blue;
+        u8 alpha;
+    };
+
+    using Pixel = RGBAu8;
+    using Image = Matrix2D<Pixel>;
+    using ImageView = MatrixView2D<Pixel>;    
+    using ImageGray = Matrix2D<u8>;
+    using GrayView = MatrixView2D<u8>;
+    
+
+    template <typename T>
+    class MatrixSubView2D
+    {
+    public:
+        T*  matrix_data_;
+        u32 matrix_width;
+
+        u32 width;
+        u32 height;
+
+        union
+        {
+            Rect2Du32 range;
+
+            struct
+            {
+                u32 x_begin;
+                u32 x_end;
+                u32 y_begin;
+                u32 y_end;
+            };
+        };
+    };
+
+
+    using SubView = MatrixSubView2D<Pixel>;    
+    using GraySubView = MatrixSubView2D<u8>;
+}
+
+
+namespace image
+{
+    constexpr inline Pixel to_pixel(u8 red, u8 green, u8 blue, u8 alpha)
+    {
+        Pixel p{};
+        p.red = red;
+        p.green = green;
+        p.blue = blue;
+        p.alpha = alpha;
+
+        return p;
+    }
+
+
+    constexpr inline Pixel to_pixel(u8 red, u8 green, u8 blue)
+    {
+        return to_pixel(red, green, blue, 255);
+    }
+
+
+    constexpr inline Pixel to_pixel(u8 gray)
+    {
+        return to_pixel(gray, gray, gray);
+    }
+}
+
+
+/* create destroy */
+
+namespace image
+{
+    bool create_image(Image& image, u32 width, u32 height);
+
+    void destroy_image(Image& image);
+}
+
+
+/* make_view */
+
+namespace image
+{
+    ImageView make_view(Image const& image);
+}
+
+
+/* sub_view */
+
+namespace image
+{
+    template <typename T>
+    static MatrixSubView2D<T> sub_view(MatrixView2D<T> const& view, Rect2Du32 const& range)
+    {
+        MatrixSubView2D<T> sub_view{};
+
+        sub_view.matrix_data_ = view.matrix_data_;
+        sub_view.matrix_width = view.width;
+        sub_view.range = range;
+        sub_view.width = range.x_end - range.x_begin;
+        sub_view.height = range.y_end - range.y_begin;
+
+        return sub_view;
+    }
+}
+
+
+/* span */
+
+namespace image
+{
+    template <typename T>
+    SpanView<T> to_span(Matrix2D<T> const& mat)
+    {
+        SpanView<T> span{};
+
+        span.begin = mat.data_;
+        span.length = mat.width * mat.height;
+
+        return span;
+    }
+
+
+    template <typename T>
+    SpanView<T> to_span(MatrixView2D<T> const& view)
+    {
+        SpanView<T> span{};
+
+        span.begin = view.matrix_data_;
+        span.length = view.width * view.height;
+
+        return span;
+    }
+
+
+    template <typename T>
+    static inline SpanView<T> row_span(MatrixView2D<T> const& view, u32 y)
+    {
+        SpanView<T> span{};
+
+        span.begin = view.matrix_data_ + (u64)(y * view.width);
+        span.length = view.width;
+
+        return span;
+    }
+
+
+    template <typename T>
+    static inline SpanView<T> row_span(MatrixSubView2D<T> const& view, u32 y)
+    {
+        SpanView<T> span{};
+
+        span.begin = view.matrix_data_ + (u64)((view.y_begin + y) * view.matrix_width + view.x_begin);
+        span.length = view.width;
+
+        return span;
+    }
+}
+
+
+/* read write */
+
+namespace image
+{    
+    bool read_image_from_file(const char* img_path_src, Image& image_dst);
+
+    bool write_image(Image const& image_src, const char* img_path_dst);
+}
---
+++ src/main.cpp
@@ -0,0 +1,224 @@
+#include "image.hpp"
+
+#include <vector>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <cstdio>
+#include <cassert>
+
+
+namespace img = image;
+
+
+constexpr u64 N_ASCII_CHARS = 95;
+
+
+constexpr auto ASCII_IMAGE_PATH = "todo/ascii_v5.png";
+
+
+static bool is_black(img::Pixel p)
+{
+    return p.alpha > 0 && p.red == 0 && p.green == 0 && p.blue == 0;
+}
+
+
+static bool is_boundary(img::Pixel p)
+{
+    return p.alpha > 0 && !is_black(p);
+}
+
+
+static void format_image(img::Image const& image)
+{
+    auto view = img::make_view(image);
+
+    auto span = to_span(image);
+    for (u32 i = 0; i < span.length; i++)
+    {
+        auto& p = span.begin[i];
+        if (!p.alpha)
+        {
+            continue;
+        }
+
+        if (p.red == p.green && p.red == p.blue)
+        {
+            p = img::to_pixel(0);
+        }
+    }
+}
+
+
+static bool validate(img::Image const& raw_ascii)
+{
+    auto view = img::make_view(raw_ascii);
+    u32 count = 0;
+
+    for (u32 y = 0; y < view.height; y++)
+    {
+        auto row = img::row_span(view, y);
+        count += is_boundary(row.begin[0]);
+    }
+
+    printf("count: %u\n", count);
+
+    return count == N_ASCII_CHARS;
+}
+
+
+std::vector<img::SubView> split_chars(img::Image const& raw_ascii)
+{
+    auto view = img::make_view(raw_ascii);
+
+    std::vector<img::SubView> list;
+
+    Rect2Du32 range{};
+    range.x_begin = 0;
+    range.y_begin = 0;
+    range.x_end = view.width;
+
+    int count = 0;
+
+    for (u32 y = 0; y < view.height; y++)
+    {
+        auto row = img::row_span(view, y);
+        if (!is_boundary(row.begin[0]))
+        {
+            range.x_end = view.width;
+            for (u32 x = 0; x < view.width; x++)
+            {
+                if (is_boundary(row.begin[x]))
+                {
+                    range.x_end = x;
+                    break;
+                }
+            }
+            continue;
+        }
+
+        count++;
+        printf("count: %u, y: %u\n", count, y);
+
+        range.y_end = y;
+
+        list.push_back(img::sub_view(view, range));
+        range.y_begin = y + 1;
+    }
+
+    assert(list.size() == N_ASCII_CHARS);
+
+    return list;
+}
+
+
+std::string to_cpp_text(img::Image const& raw_ascii)
+{
+    auto list = split_chars(raw_ascii);    
+
+    std::ostringstream oss;
+
+    oss
+    << "static const struct\n"
+    << "{\n"
+    << "    unsigned char height;\n"
+    << "    unsigned char widths[95];\n"
+    << "    const char* u8_pixel_data[95];\n"
+
+    << "}\n"
+
+    << "ascii_chars = \n"
+
+    << "{\n"
+    << "    " << list[0].height << ", // height\n\n";
+
+    oss
+    << "    { // widths\n"
+    << "        ";
+
+    for (auto const& view : list)
+    {
+        oss << view.width << ", ";
+    }
+
+    oss
+    << "\n"
+    << "    },\n\n";
+
+    oss
+    << "    { // u8_pixel_data\n";
+
+    int char_id = 32;
+    for (auto const& view : list)
+    {
+        oss << "        /* ";
+        oss << "'" << (char)(char_id++) << "'";
+        oss << " */ \"";
+
+        for (u32 y = 0; y < view.height; y++)
+        {
+            auto row = img::row_span(view, y);
+            for (u32 x = 0; x < row.length; x++)
+            {
+                oss << (row.begin[x].alpha ? 1 : 0);
+            }
+        }
+
+        oss << "\",\n";
+    }
+
+    oss
+    << "    } //\n"
+    << "};\n";
+
+    return oss.str();
+}
+
+
+bool write_to_file(std::string const& str, cstr filename)
+{
+    std::ofstream file(filename);
+
+    if (!file.is_open())
+    {
+        return false;
+    }
+
+    file << str;
+
+    file.close();
+
+    return true;
+}
+
+
+
+int main()
+{
+    img::Image image;
+    if(!img::read_image_from_file(ASCII_IMAGE_PATH, image))
+    {
+        printf("Did not read image\n");
+        return 1;
+    }
+
+    format_image(image);
+
+    if (!validate(image))
+    {
+        printf("Image not valid\n");
+        return 1;
+    }
+
+    auto const cpp_text = to_cpp_text(image);
+
+    if (!write_to_file(cpp_text, "./files/ascii_chars02.cpp"))
+    {
+        printf("Did not write file\n");
+        return 1;
+    }
+    
+    img::destroy_image(image);
+
+    return 0;
+}
